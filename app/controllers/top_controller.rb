cd1302aa6dfdd88e9f30f72b2d60d989b3304825
Kenta Murata <mrkn@mrkn.jp>
2013-11-30 13:24:52 +0900

    Add top#index

---
+++ app/controllers/top_controller.rb
@@ -0,0 +1,5 @@
+class TopController < ApplicationController
+  def index
+    render text: 'FizzBuzz JS'
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Fizzbuzzjs::Application.routes.draw do
+  root 'top#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
