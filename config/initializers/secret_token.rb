75f2a45a1085a26f20149bd816d139e5401a4487
Kenta Murata <mrkn@mrkn.jp>
2013-11-30 13:15:07 +0900

    rails new fizzbuzzjs -T -B -O

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fizzbuzzjs::Application.config.secret_key_base = 'ad5524df6423df0483e62e2e461af9d5f91d494b4ca41ccf97e2c720eec3fa22245c768059bd372a037c5f197ab947a5bf3dd6d64e68408d9ad1ab17bf5afd3e'
